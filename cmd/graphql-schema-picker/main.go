8f5b5beef3783bc906f1f06542ccef21ed7e7966
Kevin Chen <kevinmichaelchen@gmail.com>
2023-09-05 20:03:42 -0400

    feat: Add new version subcommand (#10)

--- cmd/graphql-schema-picker/main.go
+++ cmd/graphql-schema-picker/main.go
@@ -4,6 +4,16 @@ import (
 	"github.com/kevinmichaelchen/graphql-schema-picker/internal/cli"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
-	cli.Main()
+	cli.Main(cli.LDFlags{
+		Version: version,
+		Commit:  commit,
+		Date:    date,
+	})
 }
